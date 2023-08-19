f07016b6aee1b55040c4268b733d3aad5a995a94
Yujio_Nako <69411867+ldcivan@users.noreply.github.com>
2023-08-19 17:01:24 +0800

    bug_fix: https://github.com/ldcivan/ddcheck_plugin/pull/2/

--- 查成分.js
+++ 查成分.js
@@ -329,10 +329,12 @@ export class example extends plugin {
     }
 
     /** 处理描述 */
+    forwardMsg.data = JSON.stringify(forwardMsg.data)
     forwardMsg.data = forwardMsg.data
       .replace(/\n/g, '')
       .replace(/<title color="#777777" size="26">(.+?)<\/title>/g, '___')
       .replace(/___+/, `<title color="#777777" size="26">${title}</title>`)
+    forwardMsg.data = JSON.parse(forwardMsg.data)
 
     return forwardMsg
   }
